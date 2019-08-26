042ed57e6d22c9a32cede5763629105d79ae77bb
Anuarbek <anuarbekzhamanbaev@gmail.com>
2019-08-26 14:02:45 +0600

    Добавьте файлы проекта.

---
+++ ProgressBarAndCopy/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace ProgressBarAndCopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFile1 = new System.Windows.Forms.Button();
+            this.btnFile2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSourse = new System.Windows.Forms.TextBox();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFile1
+            // 
+            this.btnFile1.Location = new System.Drawing.Point(660, 58);
+            this.btnFile1.Name = "btnFile1";
+            this.btnFile1.Size = new System.Drawing.Size(75, 23);
+            this.btnFile1.TabIndex = 0;
+            this.btnFile1.Text = "Файл...";
+            this.btnFile1.UseVisualStyleBackColor = true;
+            this.btnFile1.Click += new System.EventHandler(this.btnFile1_Click);
+            // 
+            // btnFile2
+            // 
+            this.btnFile2.Location = new System.Drawing.Point(660, 111);
+            this.btnFile2.Name = "btnFile2";
+            this.btnFile2.Size = new System.Drawing.Size(75, 23);
+            this.btnFile2.TabIndex = 1;
+            this.btnFile2.Text = "Файл...";
+            this.btnFile2.UseVisualStyleBackColor = true;
+            this.btnFile2.Click += new System.EventHandler(this.btnFile2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "От куда";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Куда";
+            // 
+            // txtSourse
+            // 
+            this.txtSourse.Location = new System.Drawing.Point(164, 60);
+            this.txtSourse.Name = "txtSourse";
+            this.txtSourse.Size = new System.Drawing.Size(471, 20);
+            this.txtSourse.TabIndex = 4;
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(164, 113);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(471, 20);
+            this.txtDestination.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(94, 159);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(541, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(660, 158);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 7;
+            this.btnCopy.Text = "Копировать";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(355, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "0%";
+            this.label3.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(660, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Отмена";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.txtSourse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFile2);
+            this.Controls.Add(this.btnFile1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFile1;
+        private System.Windows.Forms.Button btnFile2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSourse;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ProgressBarAndCopy/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressBarAndCopy
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker worker = new BackgroundWorker();
+        OpenFileDialog ofd = new OpenFileDialog();
+        FolderBrowserDialog ccc = new FolderBrowserDialog();
+        public Form1()
+        {
+            InitializeComponent();
+            worker.WorkerSupportsCancellation = true;
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += Worker_DoWork;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+            worker.ProgressChanged += Worker_ProgressChanged;
+        }
+
+        private void CopyFile(string source, string destination, DoWorkEventArgs e)
+        {
+            FileStream fsOut = new FileStream(destination, FileMode.Create);
+            FileStream fsIn = new FileStream(source, FileMode.Open);
+            byte[] buffer = new byte[4096];
+            int readBytes;
+            while((readBytes = fsIn.Read(buffer,0,buffer.Length))>0)
+            {
+                if(worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    worker.ReportProgress(0);
+                    fsIn.Close();
+                    fsOut.Close();
+                    return;
+                }
+                else
+                {
+                    fsOut.Write(buffer, 0, readBytes);
+                    float prog = fsIn.Position * 100 / fsIn.Length; 
+                    worker.ReportProgress((int) prog);
+                    
+                }                
+            }
+            fsIn.Close();
+            fsOut.Close();
+        }
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if(e.Cancelled)
+            {
+                progressBar1.Visible = false;
+                MessageBox.Show("Копирование отменено", "Отменено", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                progressBar1.Visible = false;
+                MessageBox.Show("Копирование завершено", "Отменено", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            CopyFile(inputFile, outputFile + @"\" + Path.GetFileName(inputFile), e);
+        }
+
+        string inputFile = null;
+        string outputFile = null;
+        private void btnFile1_Click(object sender, EventArgs e)
+        {
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                inputFile = ofd.FileName;
+                txtSourse.Text = inputFile;
+            }
+        }
+
+        private void btnFile2_Click(object sender, EventArgs e)
+        {
+            if (ccc.ShowDialog() == DialogResult.OK)
+            {
+                outputFile = ccc.SelectedPath;
+                txtDestination.Text = outputFile + @"\" + Path.GetFileName(inputFile);
+                
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if(worker.IsBusy)
+            {
+                label3.Visible = true;
+                //label3.Text = string.Format(" {0:F1}% ", prog);
+            }
+            else
+            {
+                progressBar1.Visible = true;
+                worker.RunWorkerAsync();
+
+            }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            worker.CancelAsync();
+        }       
+    }
+}
